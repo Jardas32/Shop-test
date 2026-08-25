cde003d2339e8f2b988f65d47521846ed127fbca
Jarda <jardadjarda@gmail.com>
2026-08-25 19:52:49 +0200

    show scroll up button on scroll

--- src/components/BtnUp.jsx
+++ src/components/BtnUp.jsx
@@ -1,11 +1,39 @@
 import "../css/btnup.css";
 import { FaArrowUp } from "react-icons/fa6";
+import { useState, useEffect } from "react";
 
 function BtnUp() {
+  const [showBtn, setShowBtn] = useState(false);
+
+  useEffect(() => {
+    const showBtngoup = () => {
+      if (window.scrollY >= 160) {
+        setShowBtn(true);
+      } else {
+        setShowBtn(false);
+      }
+    };
+
+    document.addEventListener("scroll", showBtngoup);
+
+    return () => document.removeEventListener("scroll", showBtngoup);
+  }, []);
+
+  const handleGoUp = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
-    <div className="wrappet-btnup">
-      <FaArrowUp className="img-arrowup"/>
-    </div>
+    <>
+      {showBtn && (
+        <div onClick={() => handleGoUp()} className="wrappet-btnup">
+          <FaArrowUp className="img-arrowup" />
+        </div>
+      )}
+    </>
   );
 }
 
